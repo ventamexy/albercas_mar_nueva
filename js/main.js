239ff6d42a10c91a5aede18f05dd0766d112f83b
unknown <venta.mexy@gmail.com>
2022-05-30 13:06:06 -0500

    Cambios generales.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+    window.addEventListener('load', (event) => {
+        document.querySelector(".load-container").remove();
+    });
+
+});
+
 window.onload = function() {
 
     // --- Definir propieades elementos {li}
